49fed159e697293cacdac00a95c0b9b7b6383ca5
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-04-28 16:38:52 +0330

    feat:Added progress and users into modules folder.

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,9 +5,11 @@ import { AuthModule } from './auth/auth.module';
 import { ReviewModule } from './modules/review/review.module';
 import { FlashcardModule } from './modules/flashcard/flashcard.module';
 import { CollectionModule } from './modules/collection/collection.module';
+import { ProgressModule } from './modules/progress/progress.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [AuthModule, ReviewModule, FlashcardModule, CollectionModule],
+  imports: [AuthModule, ReviewModule, FlashcardModule, CollectionModule, ProgressModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
