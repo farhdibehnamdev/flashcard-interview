1a01c7c1809327db5ce2ff2cdad3d7d5925e425e
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-05-07 21:50:17 +0330

    feat:Implemented deck controller and service.

--- server/src/modules/collection/deck.controller.ts
+++ server/src/modules/collection/deck.controller.ts
@@ -1,7 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Version } from '@nestjs/common';
 import { DeckService } from './deck.service';
+import { DeckDto } from './dto/deck.dto';
 
 @Controller('deck')
 export class DeckController {
   constructor(private readonly deckService: DeckService) {}
+
+  @Version('1')
+  @Get()
+  async findAll() {
+    return await this.deckService.findAll();
+  }
+
+  @Version('1')
+  @Get()
+  async findOne(@Param('id') id: number) {
+    return await this.deckService.findOne(id);
+  }
+
+  @Version('1')
+  @Post()
+  async create(@Body() deckDto: DeckDto) {
+    return await this.deckService.create(deckDto);
+  }
 }
--- server/src/modules/collection/deck.service.ts
+++ server/src/modules/collection/deck.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import Deck from './entities/deck.entity';
+import { Repository, FindOneOptions } from 'typeorm';
+import { DeckDto } from './dto/deck.dto';
 
 @Injectable()
-export class DeckService {}
+export class DeckService {
+  constructor(
+    @InjectRepository(Deck) private readonly deckRepository: Repository<Deck>,
+  ) {}
+
+  async findAll() {
+    const [items, count] = await this.deckRepository.findAndCount();
+    return { items, count };
+  }
+
+  async findOne(id: number) {
+    const deck = await this.deckRepository.findOne({ where: { id } });
+    return deck;
+  }
+
+  async create(deckDto: DeckDto) {
+    try {
+      return await this.deckRepository.create(deckDto);
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+}
