65ee5d2caba4a1d2166dbc82ac6c9ca5dfab086a
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-05-08 17:05:02 +0330

    ✨ feat:Configure AuthModule with JWT and Guards.

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { HashingService } from './hashing/hashing.service';
+import { BcryptService } from './hashing/bcrypt.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import User from 'src/modules/users/entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import jwtConfig from 'src/config/jwt.config';
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { AccessTokenGuard } from './guards/access-token.guard';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+    ConfigModule.forFeature(jwtConfig),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [
+    { provide: HashingService, useClass: BcryptService },
+    { provide: APP_GUARD, useClass: AccessTokenGuard },
+    AuthService,
+  ],
 })
 export class AuthModule {}
