ad4647c8b6627ab00128628f78f4f76055e7db02
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-05-08 17:06:38 +0330

    ✨ feat:Implement AuthService with signup and signin methods.

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,4 +1,75 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import User from 'src/modules/users/entities/user.entity';
+import { QueryFailedError, Repository } from 'typeorm';
+import { HashingService } from './hashing/hashing.service';
+import { SignUpDto } from './dto/sign-up.dto';
+import { SignInDto } from './dto/sign-in.dto';
+import jwtConfig from 'src/config/jwt.config';
+import { ConfigType } from '@nestjs/config';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+    private readonly hashingService: HashingService,
+    private readonly jwtService: JwtService,
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+  ) {}
+  async signUp(signUpDto: SignUpDto) {
+    try {
+      const user = new User();
+      user.email = signUpDto.email;
+      user.password = await this.hashingService.hash(signUpDto.password);
+
+      await this.userRepository.save(user);
+    } catch (error) {
+      if (
+        error instanceof QueryFailedError &&
+        error.message.includes('duplicate key value violates unique constraint')
+      ) {
+        throw new ConflictException('Email address is already taken');
+      }
+      throw error;
+    }
+  }
+
+  async signIn(signInDto: SignInDto) {
+    const user = await this.userRepository.findOneBy({
+      email: signInDto.email,
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('User does not exists');
+    }
+
+    const isEqual = await this.hashingService.compare(
+      signInDto.password,
+      user.password,
+    );
+
+    if (!isEqual) {
+      throw new UnauthorizedException('Password does not match');
+    }
+    const accessToken = await this.jwtService.signAsync(
+      {
+        sub: user.id,
+        email: user.email,
+      },
+      {
+        audience: this.jwtConfiguration.audience,
+        issuer: this.jwtConfiguration.issuer,
+        secret: this.jwtConfiguration.secret,
+        expiresIn: this.jwtConfiguration.accessTokenTtl,
+      },
+    );
+    return { accessToken };
+  }
+}
